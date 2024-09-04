d9d652b6c1f2c1b31ea8db8a541126bafd8f249b
Néïs <ncros@sqli.com>
2024-09-04 20:48:49 +0200

    landing page with nzc

--- kodeonesis/src/app/components/animatedLetters.tsx
+++ kodeonesis/src/app/components/animatedLetters.tsx
@@ -6,32 +6,33 @@ interface AnimatedLettersProps {
 }
 const letterVariants = {
   Z: {
-    hidden: { opacity: 0, y: 500, rotate: 400 },
+    hidden: { opacity: 0, y: 200, rotate: 100 },
     visible: { opacity: 1, y: 0, rotate: 0, transition: { duration: 3, type: 'spring', stiffness: 100, damping: 60 } },
   },
   C: {
-    hidden: { opacity: 0, y: -500, rotate: -500 },
+    hidden: { opacity: 0, y: -200, rotate: -100 },
     visible: { opacity: 1, y: 0, rotate: 0, transition: { duration: 3, type: 'spring', stiffness: 100, damping: 60 } },
   },
   N: {
-    hidden: { opacity: 0, x: 500, rotate: -600 },
+    hidden: { opacity: 0, x: 200, rotate: -200 },
     visible: { opacity: 1, x: 0, rotate: 0, transition: { duration: 3, type: 'spring', stiffness: 100, damping: 60 } },
   },
 };
 
 const AnimatedLetters: React.FC<AnimatedLettersProps> = ({ scrollXProgress }) => {
-  const yZ = useTransform(scrollXProgress, [0, 1], [0, 500]);
-  const rotateZ = useTransform(scrollXProgress, [0, 1], [0, 400]);
+  const yZ = useTransform(scrollXProgress, [0, 1], [0, 200]);
+  const rotateZ = useTransform(scrollXProgress, [0, 1], [0, 100]);
 
-  const yC = useTransform(scrollXProgress, [0, 1], [0, -500]);
-  const rotateC = useTransform(scrollXProgress, [0, 1], [0, -500]);
+  const yC = useTransform(scrollXProgress, [0, 1], [0, -200]);
+  const rotateC = useTransform(scrollXProgress, [0, 1], [0, -100]);
 
-  const xN = useTransform(scrollXProgress, [0, 1], [0, 500]);
-  const rotateN = useTransform(scrollXProgress, [0, 1], [0, -600]);
+  const xN = useTransform(scrollXProgress, [0, 1], [0, 200]);
+  const rotateN = useTransform(scrollXProgress, [0, 1], [0, -200]);
 
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-screen w-screen">
       <div className="flex space-x-4">
+        <div className='flex flex-col justify-center items-center'>
         <motion.div
           variants={letterVariants.N}
           style={{ x: xN, rotate: rotateN }}
@@ -43,6 +44,17 @@ const AnimatedLetters: React.FC<AnimatedLettersProps> = ({ scrollXProgress }) =>
           N
         </motion.div>
         <motion.div
+          variants={letterVariants.C}
+          style={{ x: yC, rotate: rotateC }}
+
+          initial="hidden"
+          animate="visible"
+          className="text-2xl font-bold"
+        >
+          Néïs
+        </motion.div></div>
+        <div className='flex flex-col justify-center items-center'>
+        <motion.div
           variants={letterVariants.Z}
           style={{ y: yZ, rotate: rotateZ }}
 
@@ -53,6 +65,17 @@ const AnimatedLetters: React.FC<AnimatedLettersProps> = ({ scrollXProgress }) =>
           Z
         </motion.div>
         <motion.div
+          variants={letterVariants.N}
+          style={{ y: xN, rotate: rotateN }}
+
+          initial="hidden"
+          animate="visible"
+          className="text-2xl font-bold"
+        >
+          ZONCA
+        </motion.div></div>
+        <div className='flex flex-col justify-center items-center'>
+        <motion.div
           variants={letterVariants.C}
           style={{ y: yC, rotate: rotateC }}
 
@@ -62,6 +85,16 @@ const AnimatedLetters: React.FC<AnimatedLettersProps> = ({ scrollXProgress }) =>
         >
           C
         </motion.div>
+        <motion.div
+          variants={letterVariants.Z}
+          style={{ y: yZ, rotate: rotateZ }}
+
+          initial="hidden"
+          animate="visible"
+          className="text-2xl font-bold"
+        >
+          CROS
+        </motion.div></div>
         
       </div>
 
--- kodeonesis/src/app/page.tsx
+++ kodeonesis/src/app/page.tsx
@@ -4,61 +4,59 @@ import React, { useEffect, useRef, useState } from 'react';
 import AnimatedLetters from "./components/animatedLetters";
 import { motion, useScroll, useSpring } from 'framer-motion';
 import './globals.css';
-import { Navigation } from './components/navigation'
-
 
 export default function Home() {
   const scrollRef = useRef<HTMLDivElement>(null);
-  const { scrollXProgress } = useScroll({ container: scrollRef });
+  const { scrollYProgress } = useScroll({ container: scrollRef });
 
-  const scaleX = useSpring(scrollXProgress, {
+  const scaleX = useSpring(scrollYProgress, {
     stiffness: 100,
     damping: 30,
     restDelta: 0.001,
   });
 
   useEffect(() => {
-    scaleX.set(scrollXProgress.get());
-  }, [scrollXProgress, scaleX]);
+    scaleX.set(scrollYProgress.get());
+  }, [scrollYProgress, scaleX]);
 
   return (
     <div>
-    <div 
-      ref={scrollRef} 
-      className="flex-shrink-0 overflow-x-scroll h-screen w-screen scroll-smooth"
-    >
-      <Navigation />
-      <motion.div
-        className="flex"
-        style={{ width: '300vw' }}
+      <div
+        ref={scrollRef}
+        className="overflow-y-scroll h-screen w-screen scroll-smooth"
       >
-        <section 
-          className="w-screen h-screen flex items-center justify-center bg-blue-400 overflow-y-hidden"
+        <motion.div
+          className="flex flex-col"
+          style={{ height: '300vh' }}
         >
-          <AnimatedLetters scrollXProgress={scrollXProgress} />
-        </section>
+          <section
+            className="flex h-screen items-center justify-center"
+          >
+            <AnimatedLetters scrollXProgress={scrollYProgress} />
+          </section>
 
-        <section 
-          className="flex-shrink-0 w-screen h-screen overflow-y-hidden"
-        >
-          <h1 className="text-6xl text-white">Section 1</h1>
-        </section>
+          <section
+            className="flex h-screen items-center justify-center bg-red-400"
+          >
+            <h1 className="text-6xl text-white">Section 1</h1>
+          </section>
 
-        <motion.div className="relative" style={{ height: '300vh'}}>
-          <section 
-            className="flex-shrink-0 w-screen h-screen flex items-center justify-center bg-green-400 overflow-y-auto"
+          <section
+            className="flex h-screen items-center justify-center bg-green-400"
             style={{ maxHeight: '300vh' }}
           >
             <h1 className="text-6xl text-white">Section 2</h1>
           </section>
-        </motion.div>
-      </motion.div>
 
-      <motion.div 
-        className="absolute left-0 right-0 h-1 bg-red-500 bottom-24" 
-        style={{ scaleX }} 
-      />
+        </motion.div>
+
+
+
+        <motion.div
+          className="absolute left-0 right-0 h-1 bg-red-500 bottom-24"
+          style={{ scaleX }}
+        />
+      </div>
     </div>
-  </div>
   );
 }
