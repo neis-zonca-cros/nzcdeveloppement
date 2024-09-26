da6978c224d3a1df2452736510f49cfdd0afa6e9
Néïs <ncros@sqli.com>
2024-09-26 09:04:39 +0200

    MVP

--- nzcdeveloppement/src/app/components/Footer.tsx
+++ nzcdeveloppement/src/app/components/Footer.tsx
@@ -6,12 +6,11 @@ const Footer: React.FC = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="text-title text-center p-10">
+    <footer className="text-title text-center p-10 pb-safe">
       <div className="mx-auto flex flex-col text-sm md:text-base lg:text-base">
         <p className="pb-2">
           &copy; {currentYear} NZC Développement. Tous droits réservés. N°SIRET: 93271239100014
-        </p>
-        <a
+          <a
           href="https://www.linkedin.com/in/néïs-zonca-cros"
           target="_blank"
           rel="noopener noreferrer"
@@ -20,6 +19,7 @@ const Footer: React.FC = () => {
           <FaLinkedin className="mr-2" />
           LinkedIn
         </a>
+        </p>
       </div>
     </footer>
   );
