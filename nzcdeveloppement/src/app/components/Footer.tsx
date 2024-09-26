bdfd9e257aeb1801ad5252fbd17a26c0c8d62763
Néïs <ncros@sqli.com>
2024-09-26 09:11:00 +0200

    MVP

--- nzcdeveloppement/src/app/components/Footer.tsx
+++ nzcdeveloppement/src/app/components/Footer.tsx
@@ -3,26 +3,26 @@ import React from "react";
 import { FaLinkedin } from "react-icons/fa6";
 
 const Footer: React.FC = () => {
-  const currentYear = new Date().getFullYear();
+    const currentYear = new Date().getFullYear();
 
-  return (
-    <footer className="text-title text-center p-10 pb-safe">
-      <div className="mx-auto flex flex-col text-sm md:text-base lg:text-base">
-        <p className="pb-2">
-          &copy; {currentYear} NZC Développement. Tous droits réservés. N°SIRET: 93271239100014
-          <a
-          href="https://www.linkedin.com/in/néïs-zonca-cros"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="flex items-center text-sm md:text-base lg:text-base justify-center text-title hover:underline"
-        >
-          <FaLinkedin className="mr-2" />
-          LinkedIn
-        </a>
-        </p>
-      </div>
-    </footer>
-  );
+    return (
+        <footer className="text-title text-center pb-10 pb-safe">
+            <div className="mx-auto flex flex-col text-sm md:text-base lg:text-base">
+                <p className="pb-2">
+                    &copy; {currentYear} NZC Développement. Tous droits réservés. N°SIRET: 93271239100014
+                    <a
+                        href="https://www.linkedin.com/in/néïs-zonca-cros"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="flex items-center text-sm md:text-base lg:text-base justify-center text-title hover:underline"
+                    >
+                        <FaLinkedin className="mr-2" />
+                        LinkedIn
+                    </a>
+                </p>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
